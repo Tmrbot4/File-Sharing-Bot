8d78147d2a72932f0a22d599b6c4fb0495d272c8
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-06-28 17:59:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("🤖 Aʙᴏᴜᴛ 🤖", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Close 🔒", callback_data = "close")
                 ]
             ]
         )
